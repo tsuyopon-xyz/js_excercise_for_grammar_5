d07f87f34723786f522d35ff75b0badd8d9edf1b
Yuto Miura <veda.ym0209@gmail.com>
2019-02-12 12:00:46 -0800

    add functions

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+'use strict';
 // 課題1: 引数に渡した数値(整数)に応じて、奇数なら「true」、偶数なら「false」を返す「isOddNumber関数」を作る
 //   - 関数宣言の形式で作ること
 //   - 「isOddNumber関数」を定義した後に、console.logを使って「isOddNumber関数」の引数に次の数を入力して、期待している結果が返ってくること確認する
@@ -5,10 +6,17 @@
 //     - 入力: 14, 期待する結果: false
 
 // ここにisOddNumber関数を作る
-
+const isOddNumber = (num) => {
+    if (num % 2 !== 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 // ここにconsole.logを使って「9」「14」を引数に渡したときの結果値を出力する
-
+console.log('9を入力したらtrueが返るべき: ', isOddNumber(9));
+console.log('14を入力したらtrueが返るべき: ', isOddNumber(14));
 
 
 // 課題2: 引数に渡した「1」以上の整数をつかって階乗の結果を返す「factorial関数」を作る (階乗とは: https://ja.wikipedia.org/wiki/%E9%9A%8E%E4%B9%97)
@@ -22,9 +30,27 @@
 //     3. 【チャレンジ問題】再帰関数を使ったやり方(参考記事: https://qiita.com/chuck0523/items/2c40a5da90a1d73ab956)
 
 // ここにfactorial関数を作る
-
+const factorial = (num) => {
+    let result = 1;
+    for (let i = num; i > 0; i--) {
+        result *= i;
+    }
+    return result;
+}
+// recursive
+const recursiveFactorial = (num) => {
+    if (num <= 1) {
+        return num;
+    } else {
+        return num * recursiveFactorial(num - 1);
+    }
+}
 
 // ここにconsole.logを使って「4」「5」を引数に渡したときの結果値を出力する
+console.log('4を入力したら24が返るべき: ', factorial(4));
+console.log('4を入力したら24が返るべき: ', recursiveFactorial(4));
+console.log('5を入力したら120が返るべき: ', factorial(5));
+console.log('5を入力したら120が返るべき: ', recursiveFactorial(5));
 
 
 
@@ -36,6 +62,11 @@
 //   - 「先頭1文字を大文字にするやり方」の参考記事: https://qiita.com/ichironagata/items/091f8cd629b82b0dcbf8
 
 // ここにconvertFirstCharacterToUpperCase関数を作る
+const convertFirstCharacterToUpperCase = (text) => {
+    return text.charAt(0).toUpperCase() + text.slice(1);
+}
 
 
 // ここにconsole.logを使って「'hello'」「'world'」を引数に渡したときの結果値を出力する
+console.log('"hello"を入力したら、"Hello"が返るべき: ', convertFirstCharacterToUpperCase('hello'));
+console.log('"world"を入力したら、"World"が返るべき: ', convertFirstCharacterToUpperCase('world'));
